d28879b2f28c711dffa0e179d0419423c0868819
chuanqizhu0820 <chuanqizhu1991@outlook.com>
2021-08-30 16:49:53 +0100

    Adjust the font style

--- menu-icon.js
+++ menu-icon.js
@@ -1,26 +1,38 @@
 
 const icon = document.querySelector(".menu-icon");
+const header = document.querySelector(".toolbar");
 
 function expand() {
-    const list = document.querySelector(".menu-list")
+    const list = document.querySelector(".menu-list");
+
+    list.style.heigth = "800px";
+
+    list.style.display = "flex";
+    list.style.flexDirection = "column";
+
     let li1 = document.createElement('li');
     li1.id = 'portfolio';
     li1.textContent = 'Portfolio';
+    li1.style.fontSize = "24px";
+    li1.style.paddingTop = "50px";
     list.appendChild(li1);
 
     let li2 = document.createElement('li');
     li2.id = 'about';
     li2.textContent = 'About';
+    li2.style.fontSize = "24px";
+    li2.style.paddingTop = "50px";
     list.appendChild(li2);
 
     let li3 = document.createElement('li');
     li3.id = 'contact';
     li3.textContent = 'Contact';
+    li3.style.paddingTop = "50px";
+    li3.style.fontSize = "24px";
     list.appendChild(li3);
 
     const menuicon = document.querySelector("#menu-icon");
     menuicon.remove();
-
 
     const crossIcon = document.querySelector('#cross-icon');
     let listCross = document.createElement('li');
@@ -31,5 +43,10 @@ function expand() {
     listCross.appendChild(anchorCross);
 
     crossIcon.appendChild(listCross);
+
+    header.style.height = "800px";
+
+
+
 }
 icon.addEventListener('click', expand);
