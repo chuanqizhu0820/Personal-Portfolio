e3daff294011b9cd157cc625c262d914206340cb
chuanqizhu0820 <chuanqizhu1991@outlook.com>
2021-08-30 16:02:14 +0100

    Add cross icon after click menu

--- menu-icon.js
+++ menu-icon.js
@@ -24,6 +24,12 @@ function expand() {
 
     const crossIcon = document.querySelector('#cross-icon');
     let listCross = document.createElement('li');
-    
+    let anchorCross = document.createElement('a');
+    let imgCross = document.createElement("img");
+    imgCross.src = "img/mobile/cross-icon.png";
+    anchorCross.appendChild(imgCross);
+    listCross.appendChild(anchorCross);
+
+    crossIcon.appendChild(listCross);
 }
 icon.addEventListener('click', expand);
