4b95c0495ad65c1a9bd1ed51f0ac975a76eac870
chuanqizhu0820 <chuanqizhu1991@outlook.com>
2021-08-31 09:15:52 +0100

    Add menulist and close menu functions

--- menu-icon2.js
+++ menu-icon2.js
@@ -1,14 +1,30 @@
 
-const icon = document.querySelector(".menu-icon");
+const hamb = document.querySelector("#hamb-icon");
+const cross = document.querySelector("#cross-icon");
+const menu = document.querySelector("#menu-list");
+const main = document.querySelector('main');
+const logo = document.querySelector(".logo");
 
 function menuList() {
+    hamb.style.display = "none";
+    cross.style.display = "block";
+    menu.style.display = "block";
 
-    const list = document.querySelector(".menu-list");
-    const logo = document.querySelector(".logo")
-
-    icon.style.display = "none";
+    main.style.display = "none";
     logo.style.display = "none";
-    list.style.display = "block";
 }
 
-icon.addEventListener('click', menuList);
+const hambbutton = document.querySelector("#hamb-icon");
+hambbutton.addEventListener('click', menuList);
+
+function closeMenu() {
+    hamb.style.display = "block";
+    cross.style.display = "none";
+    menu.style.display = "none";
+    main.style.display = "block";
+    logo.style.display = "block";
+}
+
+const crossbutton = document.querySelector("#cross-icon");
+crossbutton.addEventListener('click', closeMenu);
+
