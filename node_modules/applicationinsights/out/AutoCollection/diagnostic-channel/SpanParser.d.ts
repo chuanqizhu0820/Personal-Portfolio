765300b8ffb5672767130c3592122781dea96cf1
chuanqizhu0820 <chuanqizhu1991@outlook.com>
2021-08-31 15:56:44 +0100

    Fix linter:

--- node_modules/applicationinsights/out/AutoCollection/diagnostic-channel/SpanParser.d.ts
+++
@@ -1,3 +0,0 @@
-import { Span } from "@opentelemetry/tracing";
-import * as Contracts from "../../Declarations/Contracts";
-export declare function spanToTelemetryContract(span: Span): (Contracts.DependencyTelemetry & Contracts.RequestTelemetry) & Contracts.Identified;
