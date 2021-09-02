0b84ca07aa66149b6e7486f92276853214f3d2ef
chuanqizhu0820 <chuanqizhu1991@outlook.com>
2021-09-02 16:51:29 +0100

    Fix linter error

--- form.js
+++ form.js
@@ -2,12 +2,12 @@ const email = document.getElementById('email');
 const form = document.querySelector('#contactform');
 
 form.addEventListener('submit', (e) => {
-    const emailinput = email.value;
-    const errorEmail = document.querySelector('small');
-    if (emailinput.toLowerCase() !== emailinput) {
-        e.preventDefault();
-        errorEmail.innerText = 'Error: please enter lower-case email address';
-    } else {
-        errorEmail.style.display = 'none';
-    }
+  const emailinput = email.value;
+  const errorEmail = document.querySelector('small');
+  if (emailinput.toLowerCase() !== emailinput) {
+    e.preventDefault();
+    errorEmail.innerText = 'Error: please enter lower-case email address';
+  } else {
+    errorEmail.style.display = 'none';
+  }
 });
